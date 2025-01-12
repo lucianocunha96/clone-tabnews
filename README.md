54d6164fdfbadc716e869614e248386ad7b08264
Luciano Cunha <lucianogcunha96@gmail.com>
2025-01-11 22:49:42 -0300

    adicionar scripts `lint:check` e `lint:fix` do prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de clone do site https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 function Home() {
-    return <h1>Finalmente rombi a barreira do comodismo e do sedentarismo ao mesmo tempo, de forma que minha mente e corpo hoje ainda estão passando juntos pelo processo doloroso, mas depois as coisas vão melhorar.</h1>
-
+  return (
+    <h1>
+      Finalmente rombi a barreira do comodismo e do sedentarismo ao mesmo tempo,
+      de forma que minha mente e corpo hoje ainda estão passando juntos pelo
+      processo doloroso, mas depois as coisas vão melhorar.
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
 
-// Voltar na aula 07 para entender melhor sobre a Vercel e 
+// Voltar na aula 07 para entender melhor sobre a Vercel e
 // criar a conta para lançar o site na web.
