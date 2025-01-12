44e9ad37f53cd4d351afd939c2fc2dcd330325a5
Luciano Cunha <lucianogcunha96@gmail.com>
2025-01-11 22:33:58 -0300

    adicionar arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
     return <h1>Finalmente rombi a barreira do comodismo e do sedentarismo ao mesmo tempo, de forma que minha mente e corpo hoje ainda estão passando juntos pelo processo doloroso, mas depois as coisas vão melhorar.</h1>
+
 }
 
 export default Home
+
+// Voltar na aula 07 para entender melhor sobre a Vercel e 
+// criar a conta para lançar o site na web.
